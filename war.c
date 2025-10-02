063118e551fcc2cc65fcd6536ff999e2d0ffe4dd
VitoriaThaisJanuario <202502562861@alunos.estacio.br>
2025-10-02 13:18:14 -0300

    codigo corrigido war.c

--- war.c
+++ war.c
@@ -8,41 +8,39 @@ typedef struct {
     char nome[50];
     char corExercito[10];
     int tropas;
-} Território;
+} Territorio;
 
 int main() {
     Territorio territorios[QTD_TERRITORIOS];
 
-    printf("=== Quantidade de Territorios ===");
+    printf("=== Cadastro de Territorios ===");
 
 // Cadastro dos 5 territórios
-for (int i= 0; i< QTD_TERRITORIOS; i++) {
-    printf("Territorio %d:\n", i +1);
+for (int i = 0; i < QTD_TERRITORIOS; i++) {
+    printf("\nTerritorio %d:\n", i +1);
 
     printf("Nome do territorio: ");
-    scanf(" %[^\n]", territorios[i].nome);  //le string com espaço
+    scanf(" %49[^\n]", territorios[i].nome);  //le string com espaço
 
     printf("Cor do exercito: ");
-    scanf(" %[^\n]", territorios[i].corExercito);
+    scanf(" %9[^\n]", territorios[i].corExercito);
 
     printf("Quantidade de tropas: ");
-    scanf("d, &territorios[i].tropas);
+    scanf("%d", &territorios[i].tropas);
 
-    printf("\n")
-    }
+    printf("\n");
+    };
 
 //Exibição dos dados cadastrados
-print("\n=== Territorios Cadastrados ===\n");
-for (int i =0 <QTD_TERRITORIOS; i + 1) {
-    printf("\nTerritorio %d:\n", i +1);
+    printf("\n=== Territorios Cadastrados ===\n");
+for (int i = 0; i < QTD_TERRITORIOS; i++) {
+    printf("\nTerritorio %d:\n", i + 1);
     printf("Nome: %s\n", territorios[i].nome);
     printf("Cor do exercito : %s\n", territorios[i].corExercito);
     printf("Quantidade de tropas: %d\n", territorios[i].tropas);
 }
  return 0;
 }
-
-
 
 
 
